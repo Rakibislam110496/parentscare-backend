d14204f732ba74849e4619cda9065f9452f2d27e
paragKhan <prgkhan@gmail.com>
2022-01-26 23:25:44 +0600

    Otp sending bug fixed

--- app/Http/Controllers/SMSOTPController.php
+++ app/Http/Controllers/SMSOTPController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\OTP;
 use Carbon\Carbon;
+use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 use Nette\Utils\Random;
 
 class SMSOTPController extends Controller
 {
-    public function sendMessage($to, $message){
+    public function sendMessage($to, $message)
+    {
         $username = env('MOBIREACH_USERNAME');
         $password = env('MOBIREACH_PASSWORD');
         $from = env('MOBIREACH_FROM');
@@ -18,29 +21,35 @@ class SMSOTPController extends Controller
         $fullApiUrl = "https://api.mobireach.com.bd/SendTextMessage?Username={$username}&Password={$password}&From={$from}&To={$to}&Message={$message}";
 
         $client = new Client();
-        $response = $client->request('GET', $fullApiUrl);
-        $apiResponse = ['statusCode'=>$response->getStatusCode(),'reasonPhrase'=>$response->getReasonPhrase(),'serverResponse'=>$response->getBody()->getContents()];
 
-        return json_decode(json_encode(simplexml_load_string($apiResponse['serverResponse'])))->ServiceClass;;
+        try {
+            $client->request('GET', $fullApiUrl);
+        } catch (GuzzleException $exception) {
+            return false;
+        }
+
+        return true;
     }
 
-    public function sendOTP(){
+    public function sendOTP()
+    {
         $otp = auth()->user()->otps()->create(['token' => Random::generate(4, '0-9')]);
 
-        $this->sendMessage(auth()->user()->phone, "Your Parents Care One-Time PIN is {$otp->token}. It will expire in 10 minutes.");
+        $res = $this->sendMessage(auth()->user()->phone, "Your Parents Care One-Time PIN is {$otp->token}. It will expire in 10 minutes.");
 
-        return response()->json(['message' => 'Please check your inbox for OTP']);
+        return response()->json(['message' => $res ? 'Please check your inbox for OTP' : "An OTP could not be sent"], $res ? 200 : 500);
     }
 
-    public function verifyOTP(Request $request){
+    public function verifyOTP(Request $request)
+    {
 
         $validated = $request->validate([
-           'token' => 'string|min:4|max:4'
+            'token' => 'string|min:4|max:4'
         ]);
 
         $otp = auth()->user()->otps()->where('token', $validated['token'])->first();
 
-        if(!$otp || Carbon::now()->gt(Carbon::parse($otp->created_at)->addMinutes(10))){
+        if (!$otp || Carbon::now()->gt(Carbon::parse($otp->created_at)->addMinutes(10))) {
             return response()->json(['message' => 'Invalid OTP'], 422);
         }
 
